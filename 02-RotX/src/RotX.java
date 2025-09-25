0349b28aef32a66e778701271366fbaac2f1721b
Jime1805 <18ejimen@gmail.com>
2025-09-25 19:04:12 +0200

    Rot13 millor no el toco

--- 02-RotX/src/RotX.java
+++ 02-RotX/src/RotX.java
@@ -1,3 +1,18 @@
 public class RotX {
-    
+
+    public static void main(String[] args){
+
+    }
+
+    public static String xifraRotX(String cadena, int desplacament){
+        codificador(cadena, desplacament, true);
+    }
+
+    public static String desxifraRotX(String cadena, int desplacament){
+        codificador(cadena, desplacament, false);
+    }
+
+    public static codificador(String cadena, int desplacament, boolean xifratge){
+        
+    }
 }
