7d5433c9a905a31d04d5e2688a93e7d16e8abd72
Jime1805 <18ejimen@gmail.com>
2025-09-26 16:05:44 +0200

    feina acabada

--- 02-RotX/src/RotX.java
+++ 02-RotX/src/RotX.java
@@ -1,23 +1,112 @@
 public class RotX {
 
     public static void main(String[] args){
-        forcaBruta("Hola");
+        forcaBrutaRotX("Óvùh");
+        System.out.println(xifraRotX("Hola", 13));
+        System.out.println(desxifraRotX("Óvùh", 13));
     }
 
     public static String xifraRotX(String cadena, int desplacament){
-        return codificador(cadena, desplacament, true);
+        return codificador(cadena, desplacament, true, false);
     }
 
     public static String desxifraRotX(String cadena, int desplacament){
-        return codificador(cadena, desplacament, false);
+        return codificador(cadena, desplacament, false, false);
     }
 
-    public static void forcaBruta(String cadena){
+    public static void forcaBrutaRotX(String cadena){
+        codificador(cadena, 0, false, true);
+    }
+    
+    public static String codificador(String cadena, int desplacament, boolean xifratge, boolean forca){
+
         String result = "";
         char[] min = "aäáàbcdeëéèfghiïìíjklmnoöòópqrstuùúüvwxyz".toCharArray(); // 41
         char[] may = "AÄÁÀBCDEËÉÈFGHIÏÌÍJKLMNOÖÒÓPQRSTUÙÚÜVWXYZ".toCharArray(); // 41
 
-        for (int k = 0; k < 41; k++){
+        if (forca == true){
+            System.out.println();
+            System.out.println("Sistema per força bruta:");
+            System.out.println("------------------------------------");
+
+            for (int k = 0; k < 41; k++){
+                result = "";
+                for (int i = 0; i < cadena.length(); i++){
+                    char caracter = cadena.charAt(i);
+
+                    for (int j = 0; j < may.length; j++){
+
+                        char pos_may = may[j];
+
+                        if (pos_may == caracter){
+                            if ((j - k) < 0){
+                                result = result + may[Math.abs(41 + j - k)];
+                            }
+                            else{
+                                result = result + may[Math.abs(j - k)];
+                            }
+                        }
+                    }
+
+                    for (int j = 0; j < min.length; j++){
+                        char pos_min = min[j];
+                        if (pos_min == caracter){
+                            if ((j - k) < 0){
+                                result = result + min[Math.abs(41 + j - k)];
+                            }
+                            else{
+                                result = result + min[Math.abs(j - k)];
+                            }
+                        }
+                    }
+                }
+                
+                System.out.println("Número fet ús: " + k + " " + result);
+            }
+            return "";
+        }
+        else{
+            if (xifratge == true){
+                System.out.println();
+                System.out.println("Xifratge per desplaçament:");
+                System.out.println("------------------------------------");
+                for (int i = 0; i < cadena.length(); i++){
+                    char caracter = cadena.charAt(i);
+
+                    for (int j = 0; j < may.length; j++){
+
+                        char pos_may = may[j];
+
+                        if (pos_may == caracter){
+                            if (Math.abs(j + desplacament)>=41){
+                                result = result + may[Math.abs(41 - j - desplacament)];
+                            }
+                            else{
+                                result = result + may[Math.abs(j + desplacament)];
+                            }
+                        }
+                    }
+
+                    for (int j = 0; j < min.length; j++){
+                        char pos_min = min[j];
+                        if (pos_min == caracter){
+                            if (Math.abs(j + desplacament)>=41){
+                                result = result + min[Math.abs(41 - j - desplacament)];
+                            }
+                            else{
+                                result = result + min[Math.abs(j + desplacament)];
+                            }
+                        }
+                    }
+
+                }
+                System.out.println("Còdig xifrat");
+                return result;
+            }
+            System.out.println();
+            System.out.println("Xifratge per desplaçament:");
+            System.out.println("------------------------------------");
+
             for (int i = 0; i < cadena.length(); i++){
                 char caracter = cadena.charAt(i);
 
@@ -26,11 +115,11 @@ public class RotX {
                     char pos_may = may[j];
 
                     if (pos_may == caracter){
-                        if ((j - k) < 0){
-                            result = result + may[Math.abs(41 + j - k)];
+                        if ((j - desplacament) < 0){
+                            result = result + may[Math.abs(41 + j - desplacament)];
                         }
                         else{
-                            result = result + may[Math.abs(j - k)];
+                            result = result + may[Math.abs(j - desplacament)];
                         }
                     }
                 }
@@ -38,130 +127,17 @@ public class RotX {
                 for (int j = 0; j < min.length; j++){
                     char pos_min = min[j];
                     if (pos_min == caracter){
-                        if ((j - k) < 0){
-                            result = result + min[Math.abs(41 + j - k)];
+                        if ((j - desplacament) < 0){
+                            result = result + min[Math.abs(41 + j - desplacament)];
                         }
                         else{
-                            result = result + min[Math.abs(j - k)];
+                            result = result + min[Math.abs(j - desplacament)];
                         }
                     }
                 }
             }
             System.out.println("Còdig desxifrat");
-            System.out.println(result);
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    public static String codificador(String cadena, int desplacament, boolean xifratge){
-
-        String result = "";
-        char[] min = "aäáàbcdeëéèfghiïìíjklmnoöòópqrstuùúüvwxyz".toCharArray(); // 41
-        char[] may = "AÄÁÀBCDEËÉÈFGHIÏÌÍJKLMNOÖÒÓPQRSTUÙÚÜVWXYZ".toCharArray(); // 41
-
-        if (xifratge == true){
-            for (int i = 0; i < cadena.length(); i++){
-                char caracter = cadena.charAt(i);
-
-                for (int j = 0; j < may.length; j++){
-
-                    char pos_may = may[j];
-
-                    if (pos_may == caracter){
-                        if (Math.abs(j + desplacament)>=41){
-                            result = result + may[Math.abs(41 - j - desplacament)];
-                        }
-                        else{
-                            result = result + may[Math.abs(j + desplacament)];
-                        }
-                    }
-                }
-
-                for (int j = 0; j < min.length; j++){
-                    char pos_min = min[j];
-                    if (pos_min == caracter){
-                        if (Math.abs(j + desplacament)>=41){
-                            result = result + min[Math.abs(41 - j - desplacament)];
-                        }
-                        else{
-                            result = result + min[Math.abs(j + desplacament)];
-                        }
-                    }
-                }
-
-            }
-            System.out.println("Còdig xifrat");
             return result;
         }
-        for (int i = 0; i < cadena.length(); i++){
-            char caracter = cadena.charAt(i);
-
-            for (int j = 0; j < may.length; j++){
-
-                char pos_may = may[j];
-
-                if (pos_may == caracter){
-                    if ((j - desplacament) < 0){
-                        result = result + may[Math.abs(41 + j - desplacament)];
-                    }
-                    else{
-                        result = result + may[Math.abs(j - desplacament)];
-                    }
-                }
-            }
-
-            for (int j = 0; j < min.length; j++){
-                char pos_min = min[j];
-                if (pos_min == caracter){
-                    if ((j - desplacament) < 0){
-                        result = result + min[Math.abs(41 + j - desplacament)];
-                    }
-                    else{
-                        result = result + min[Math.abs(j - desplacament)];
-                    }
-                }
-            }
-        }
-        System.out.println("Còdig desxifrat");
-        return result;
-    }
-
-    public static void codificadorforca(String cadena){
-        
     }
 }
